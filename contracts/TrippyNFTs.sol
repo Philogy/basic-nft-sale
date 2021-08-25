f99b413c629e38f25a9d62102f5074917730dc6d
MrClottom <philippe@dumo.net>
2021-08-26 00:14:46 +0200

    Implement basic whitelist buying

--- contracts/TrippyNFTs.sol
+++ contracts/TrippyNFTs.sol
@@ -2,20 +2,23 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TrippyNFTs is ERC721URIStorage, Ownable {
+    using SignatureChecker for address;
+
     struct SaleParams {
         uint64 price;
         uint32 start;
         uint32 end;
-        uint32 maxBuys;
+        uint64 userMaxBuys;
         uint64 totalMaxBuys;
     }
 
     struct Sale {
         SaleParams params;
+        uint256 totalBuys;
         mapping(address => uint256) buys;
     }
 
@@ -30,39 +33,72 @@ contract TrippyNFTs is ERC721URIStorage, Ownable {
     bytes32 internal constant DS_VALID_METADATA =
         0x7f0194f46a516c4bd3380098cb2274989108caa7eb882bf30e7d9807f42d7003;
 
-    address internal verifier;
-
     Sale public whitelistedSale;
     Sale public publicSale;
+    uint256 public totalBuys;
+    uint256 public totalIssued;
+    uint256 public immutable maxTotal;
+
+    address internal verifier;
 
     constructor(
         string memory name_,
         string memory symbol_,
         SaleParams memory _whitelistedSaleParams,
+        uint256 _maxTotal,
         address _verifier
     )
         ERC721(name_, symbol_)
         Ownable()
     {
         whitelistedSale.params = _whitelistedSaleParams;
+        maxTotal = _maxTotal;
         verifier = _verifier;
     }
 
-    function doWhitelistBuy(bytes memory _whitelistedSig) external payable {
-        (
-            uint256 newBuys,
-            uint256 newTotalBuys,
-            uint256 refund
-        ) = _checkSale(whitelistedSale, msg.value, msg.sender);
+    function setVerifier(address _newVerifier) external onlyOwner {
+        verifier = _newVerifier;
     }
 
-    function _checkSale(Sale storage _sale, uint256 _msgValue, address _buyer)
-        internal
-        view
-        returns (uint256 newBuys, uint256 newTotalBuys, uint256 refund)
+    function withdrawProceedsTo(address payable _recipient, uint256 _amount)
+        external onlyOwner
     {
-        _checkTime(_sale.params);
-        (newBuys, newTotalBuys, refund) = _checkValue(_sale, msgValue, buyer);
+        _recipient.transfer(_amount);
+    }
+
+    function allocate(address _recipient, uint256 _amount) external onlyOwner {
+        _mintMany(_recipient, _amount);
+    }
+
+    function doWhitelistBuy(bytes memory _whitelistedSig) external payable {
+        _checkTime(whitelistedSale.params);
+        _verifyWhitelist(msg.sender, _whitelistedSig);
+        uint256 toBeBought = msg.value / uint256(whitelistedSale.params.price);
+        require(toBeBought >= 1, "TrippyNFTs: must buy atleast 1");
+        uint256 userTotalBuys = whitelistedSale.buys[msg.sender] + toBeBought;
+        require(
+            userTotalBuys <= whitelistedSale.params.userMaxBuys,
+            "TrippyNFTs: buys maxed out"
+        );
+        uint256 totalBuys = whitelistedSale.totalBuys + toBeBought;
+        require(
+            totalBuys <= whitelistedSale.params.totalMaxBuys,
+            "TrippyNFTs: whitelisted sold out"
+        );
+        _mintMany(msg.sender, toBeBought);
+        totalBuys += toBeBought;
+        whitelistedSale.buys[msg.sender] = userTotalBuys;
+        whitelistedSale.totalBuys = totalBuys;
+    }
+
+    function _mintMany(address _recipient, uint256 _amount) internal {
+        uint256 totalIssued_ = totalIssued;
+        uint256 issued = totalIssued_ + _amount;
+        require(issued <= maxTotal, "TrippyNFTs: max issued");
+        for (uint256 i; i < _amount; i++) {
+            _safeMint(_recipient, totalIssued_ + i);
+        }
+        totalIssued = issued;
     }
 
     function _checkTime(SaleParams storage _params) internal {
@@ -71,17 +107,15 @@ contract TrippyNFTs is ERC721URIStorage, Ownable {
         require(timestamp <= _params.end, "TrippyNFTs: after sale");
     }
 
-    function _checkValue(Sale storage _sale, uint256 _msgValue, address _buyer)
+    function _verifyWhitelist(address _account, bytes memory _whitelistedSig)
         internal
-        view
-        returns (uint256 newBuys, uint256 newTotalBuys, uint256 refund)
     {
-        require(_msgValue > 0, "TrippyNFTs: no funds");
-        uint256 price = uint256(_sale.params.price);
-        uint256 maxBuys = uint256(_sale.params.maxBuys);
-        newBuys = _msgValue / price;
-        newTotalBuys = _sale.buys[_buyer] + newBuys;
-        require(newTotalBuys <= maxBuys, "TrippyNFTs: can't buy more");
-        refund = _msgValue % price;
+        require(
+            verifier.isValidSignatureNow(
+                keccak256(abi.encode(DS_IS_WHITELISTED, _account)),
+                _whitelistedSig
+            ),
+            "TrippyNFTs: not whitelisted"
+        );
     }
 }
